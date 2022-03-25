b482b04afd9b8b175a89ad15d24f5a1672513732
Chae hee chan <eqholic1125@gmail.com>
2022-03-25 20:12:47 +0900

    feat: channels 라이브러리 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chat',
+    'channels',
 ]
+ASGI_APPLICATION = 'mysite.routing.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
