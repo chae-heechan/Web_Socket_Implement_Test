7c637401d945028f76df53292bae613051d6a125
Chae hee chan <eqholic1125@gmail.com>
2022-03-25 19:47:48 +0900

    feat: 웹소켓 연결

--- mysite/settings.py
+++ mysite/settings.py
@@ -70,6 +70,14 @@ TEMPLATES = [
 ]
 
 ASGI_APPLICATION = 'mysite.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
