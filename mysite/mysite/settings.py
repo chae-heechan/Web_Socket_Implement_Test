d55ad7d63310a44899ab138ad6dae67bfbd16832
Chae hee chan <eqholic1125@gmail.com>
2022-03-26 14:56:17 +0900

    동기방식으로 채팅 구현

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,5 +126,13 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#Channels
+# Channels
 ASGI_APPLICATION = 'mysite.asgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
